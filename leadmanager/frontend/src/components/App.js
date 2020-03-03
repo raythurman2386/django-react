ade12906ccd1dc15931d687d8872bc86bbb38d62
raythurman2386 <raymondthurman5@gmail.com>
2020-03-03 18:42:20 -0500

    minor refactor to app component

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDom from 'react-dom'
 
 class App extends React.Component {
 	render() {
@@ -7,4 +6,4 @@ class App extends React.Component {
 	}
 }
 
-ReactDom.render(<App />, document.getElementById('root'))
+export default App
