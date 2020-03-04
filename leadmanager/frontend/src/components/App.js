af051d2c9dcc2aa007228c1ce1da51ddba133de8
raythurman2386 <raymondthurman5@gmail.com>
2020-03-03 21:33:40 -0500

    fetching leads from the backend

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-class App extends React.Component {
-	render() {
-		return <h1>Hello</h1>
-	}
+const App = () => {
+	const [leads, setLeads] = React.useState([])
+
+	React.useEffect(() => {
+		fetch('http://localhost:8000/api/leads')
+			.then(res => {
+				return res.json()
+			})
+			.then(data => {
+				setLeads(data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [])
+
+	return leads.map(lead => <h1 key={lead.id}>{lead.name}</h1>)
 }
 
 export default App
