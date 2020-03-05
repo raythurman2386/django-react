9ade3ac2eb32557ebe837b202d821c5ea4cdec82
raythurman2386 <raymondthurman5@gmail.com>
2020-03-04 22:28:30 -0500

    fixed delete issues with rerendering

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,4 +1,5 @@
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types'
+import axios from 'axios'
 
 export const getLeads = () => dispatch => {
 	fetch('http://localhost:8000/api/leads/')
@@ -15,32 +16,34 @@ export const getLeads = () => dispatch => {
 }
 
 export const deleteLead = id => dispatch => {
-	fetch(`http://localhost:8000/api/leads/${id}/`, {
-		method: 'DELETE'
-	})
-		.then(data => {
+	axios
+		.delete(`http://localhost:8000/api/leads/${id}/`)
+		.then(res => {
 			dispatch({
 				type: DELETE_LEAD,
-				payload: id
+				payload: { id }
 			})
 		})
 		.catch(err => console.log(err))
 }
 
 export const addLead = lead => dispatch => {
-	fetch(`http://localhost:8000/api/leads/`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(lead)
-	})
-		.then(res => res.json())
-		.then(data => {
+	axios
+		.post('http://localhost:8000/api/leads/', lead)
+		.then(res => {
 			dispatch({
 				type: ADD_LEAD,
-				payload: data
+				payload: res.data
 			})
 		})
-		.catch(err => console.log(err))
+		.catch(err => {
+			const errors = {
+				msg: err.response.data,
+				status: err.response.status
+			}
+			dispatch({
+				type: GET_ERRORS,
+				payload: errors
+			})
+		})
 }
