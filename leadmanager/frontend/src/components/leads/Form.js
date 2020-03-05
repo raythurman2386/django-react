58e04fceca3d402de8414f54e1bf6370035ab589
raythurman2386 <raymondthurman5@gmail.com>
2020-03-04 21:16:42 -0500

    form created and sending data

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,11 +1,67 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { addLead } from '../../actions/leads'
 
-const Form = () => {
+const Form = ({ addLead }) => {
+	const [name, setName] = React.useState('')
+	const [email, setEmail] = React.useState('')
+	const [message, setMessage] = React.useState('')
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		addLead({ name, email, message })
+		setName('')
+		setEmail('')
+		setMessage('')
+	}
+
 	return (
-		<form>
-			<h1>Hi</h1>
-		</form>
+		<div className='card card-body mt-4 mb-4'>
+			<h2>Add Lead</h2>
+			<form onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<label>Name</label>
+					<input
+						className='form-control'
+						type='text'
+						name='name'
+						onChange={e => setName(e.target.value)}
+						value={name}
+					/>
+				</div>
+				<div className='form-group'>
+					<label>Email</label>
+					<input
+						className='form-control'
+						type='email'
+						name='email'
+						onChange={e => setEmail(e.target.value)}
+						value={email}
+					/>
+				</div>
+				<div className='form-group'>
+					<label>Message</label>
+					<textarea
+						className='form-control'
+						type='text'
+						name='message'
+						onChange={e => setMessage(e.target.value)}
+						value={message}
+					/>
+				</div>
+				<div className='form-group'>
+					<button type='submit' className='btn btn-primary'>
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
 	)
 }
 
-export default Form
+// propTypes = {
+// 	addLead: PropTypes.func.isRequired
+// }
+
+export default connect(null, { addLead })(Form)
