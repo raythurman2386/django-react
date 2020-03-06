c03d9b8f4e93a0ee47a3eda3c11e7ca46d9c2ffe
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-06 00:42:08 -0500

    login route working correctly

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -32,4 +32,16 @@ class Login(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
 # Get User
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
--- leadmanager/accounts/urls.py
+++ leadmanager/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .api import Registration, Login
+from .api import Registration, Login, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', Registration.as_view()),
     path('api/auth/login', Login.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
 ]
