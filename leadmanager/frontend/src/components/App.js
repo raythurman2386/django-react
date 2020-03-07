5ccce60e3f09716f8ebc7f06c34b40dcbbb96554
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-06 21:50:08 -0500

    Private route component created and implemented

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+import Register from './accounts/Register'
+import Login from './accounts/Login'
+import PrivateRoute from './utils/PrivateRoute'
 import { Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 import Alerts from './layout/Alerts'
-import Register from './accounts/Register'
-import Login from './accounts/Login'
 
 const alertOptions = {
 	timeout: 3000,
@@ -21,7 +22,7 @@ const App = () => {
 				<Alerts />
 				<div className='container'>
 					<Switch>
-						<Route exact path='/' component={Dashboard} />
+						<PrivateRoute exact path='/' component={Dashboard} />
 						<Route path='/register' component={Register} />
 						<Route path='/login' component={Login} />
 					</Switch>
